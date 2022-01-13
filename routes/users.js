ce4acaf3a9c06cde3f4b3d13ec9ca0c4488b83c5
unknown <juheri842@gmail.com>
2022-01-13 14:55:07 +0700

    add env example and add route users

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,23 @@ const validate = require("../helpers/validate");
 module.exports = (app) => {
     app.route("/register").post(user.Register);
     app.route("/login").post(user.Login);
+    app.route("/company/user").post(user.addUser);
+
     app.route("/description").put(user.Description);
-    app.route("/logo").put(Multer({ storage: multer_config.diskStorage(), fileFilter: validate.imageFilter }).single("image"), user.Logo);
-    app.route("/avatar").put(Multer({ storage: multer_config.diskStorage(), fileFilter: validate.imageFilter }).single("image"), user.Avatar);
-}
+    app.route("/logo").put(
+        Multer({
+            storage: multer_config.diskStorage(),
+            fileFilter: validate.imageFilter,
+        }).single("image"),
+        user.Logo
+    );
+    app.route("/avatar").put(
+        Multer({
+            storage: multer_config.diskStorage(),
+            fileFilter: validate.imageFilter,
+        }).single("image"),
+        user.Avatar
+    );
+
+    app.route("/user/destroy").delete(user.deleteUser);
+};
